e0256cd400764e4d21133c60b7dd2e6612449cd3
Ivan_Peskov <ipeskov@gmail.com>
2023-04-25 11:07:38 +0300

    Otvet 8.1

--- Zadacha_8/Otvet/Program.cs
+++ Zadacha_8/Otvet/Program.cs
@@ -1,3 +1,16 @@
 ﻿//Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N.
 
+Console.Write (" Enter namber: - ");
+int namber = Convert.ToInt32 ( Console.ReadLine ());
+
+int evenNamber = 2;
+
+if ( namber > 1)
+{
+    while ( evenNamber <= namber)
+    {
+        Console.Write ( evenNamber + " ");
+        evenNamber = evenNamber +2;
+    }
+}
