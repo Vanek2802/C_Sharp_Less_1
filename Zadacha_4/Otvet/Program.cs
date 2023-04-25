3e9fd370720ad9db85fcfcc3923b322683862e7c
Ivan_Peskov <ipeskov@gmail.com>
2023-04-25 10:14:00 +0300

    Otvet 4.1

--- Zadacha_4/Otvet/Program.cs
+++ Zadacha_4/Otvet/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход три числа
 // и выдаёт максимальное из этих чисел.
 
+Console.WriteLine ( " Enter First namber: - ");
+int FirstNamber = Convert.ToInt32 ( Console.ReadLine () );
+
+Console.WriteLine ( " Enter Second namber: - ");
+int SecondNamber = Convert.ToInt32 ( Console.ReadLine () );
+
+Console.WriteLine ( " Enter Third namber: - ");
+int ThirdNamber = Convert.ToInt32 ( Console.ReadLine () );
