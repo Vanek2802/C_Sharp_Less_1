61240a7a73c8fa325ed9316e46ee76b38d22542b
Ivan_Peskov <ipeskov@gmail.com>
2023-04-25 09:44:33 +0300

    Otvet 2.0

--- Zadacha_2/Otvet/Program.cs
+++ Zadacha_2/Otvet/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+int max = 0;
+int min = 0;
+
+Console.Write ("Enter First namber -");
+int FirstNamber = Convert.ToInt32 (Console.ReadLine());
+
