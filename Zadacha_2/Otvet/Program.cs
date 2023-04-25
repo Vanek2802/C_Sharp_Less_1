b297bd597d032a53d6b47f2965145e6bd9e105c5
Ivan_Peskov <ipeskov@gmail.com>
2023-04-25 10:07:08 +0300

    Otvet 2.3

--- Zadacha_2/Otvet/Program.cs
+++ Zadacha_2/Otvet/Program.cs
@@ -20,4 +20,4 @@ else
     max = SecondNamber;
     min = FirstNamber;
 }
-Console.WriteLine ( " max = " + max + " , " + " min = " + min );
+Console.WriteLine ( " Otvet: max = " + max + " , " + " min = " + min );
